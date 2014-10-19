f88d46c36ff0d4f56dc555c925e47004c4fd8451
Pierre-Luc Dion <pdion891@apache.org>
2014-10-19 15:42:56 -0400

    output default config: doc format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'defined'
-
+  config.formatter = 'documentation'
+  config.color = true
 end
 
