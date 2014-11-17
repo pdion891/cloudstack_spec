6fef9c7fbd7c3a386b3f5088ace01bb2681a80d4
Pierre-Luc Dion <pdion891@apache.org>
2014-11-16 19:47:34 -0500

    cloudstack connection with ssl option

--- lib/cloudstack_spec/helper/api.rb
+++ lib/cloudstack_spec/helper/api.rb
@@ -3,15 +3,15 @@ module CloudstackSpec::Helper
   class Api
     def connection
       configs     = YAML.load_file("spec/config.yml")
-      _host       = configs['cloudstack']['host']
-      _port       = configs['cloudstack']['port']
-      _admin_port = configs['cloudstack']['admin_port']
+      _url        = configs['cloudstack']['url']
       _api_key    = configs['cloudstack']['api_key']
       _secret_key = configs['cloudstack']['secret_key']
+      _use_ssl    = configs['cloudstack']['use_ssl']
       @client     = CloudstackRubyClient::Client.new(
-                    "http://#{_host}:#{_port}/client/api",
+                    "#{_url}",
                     "#{_api_key}",
-                    "#{_secret_key}")
+                    "#{_secret_key}", 
+                    _use_ssl)
     end
   
     def url
